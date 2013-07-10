0f15807ecdbd8ca713b35c7543499a1530d700e8
pgonzalezAlivebox <pgonzalez@alivebox.com>
2013-07-10 11:35:44 -0600

    create second template

--- js/app.js
+++ js/app.js
@@ -20,7 +20,16 @@ var cases = [
         description: 'Boland et al v. Yoccabel Construction Company, Inc.',
         parties: '10',
         documents:'5',
-        announcements:'2'
+        announcements:'2',
+        recentActivities: [
+            {id:'0', date:'APR 23', description:'17 people are added to the case'},
+            {id:'1', date:'APR 20', description:'3 files were saved to the attorneys'},
+            {id:'2', date:'APR 19', description:'Timothy MacArthur requested John VanHousen be added'}],
+        notifications: [
+            {id:'0', description:'New party member: John Smith has been added'},
+            {id:'1', description:'Scoott Justice, Court Clerk posted: The court will be closed monday'},
+            {id:'2', description:'New party member: Carrie Carpenter has been added'}],
+        summary:{title:'B vs Y', days:'28 Days', description:'til next hearing'}
     },
     {
         id: 1,
@@ -28,7 +37,16 @@ var cases = [
         description: 'Tenempaguy v. 160 Water Street, Inc.',
         parties: '23',
         documents:'9',
-        announcements:'10'
+        announcements:'10',
+        recentActivities: [
+            {id:'0', date:'APR 23', description:'17 people are added to the case'},
+            {id:'1', date:'APR 20', description:'3 files were saved to the attorneys'},
+            {id:'2', date:'APR 19', description:'Timothy MacArthur requested John VanHousen be added'}],
+        notifications: [
+            {id:'0', description:'New party member: John Smith has been added'},
+            {id:'1', description:'Scoott Justice, Court Clerk posted: The court will be closed monday'},
+            {id:'2', description:'New party member: Carrie Carpenter has been added'}],
+        summary:{title:'T vs W', days:'28 Days', description:'til next hearing'}
     },
     {
         id: 2,
@@ -36,7 +54,16 @@ var cases = [
         description: 'Jones V. Grey',
         parties: '36',
         documents:'11',
-        announcements:'3'
+        announcements:'3',
+        recentActivities: [
+            {id:'0', date:'APR 23', description:'17 people are added to the case'},
+            {id:'1', date:'APR 20', description:'3 files were saved to the attorneys'},
+            {id:'2', date:'APR 19', description:'Timothy MacArthur requested John VanHousen be added'}],
+        notifications: [
+            {id:'0', description:'New party member: John Smith has been added'},
+            {id:'1', description:'Scoott Justice, Court Clerk posted: The court will be closed monday'},
+            {id:'2', description:'New party member: Carrie Carpenter has been added'}],
+        summary:{title:'J vs G', days:'28 Days', description:'til next hearing'}
     }
 
 ];;
