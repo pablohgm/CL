975c10129f462658729660f4b27d4f816c22fa2e
pgonzalezAlivebox <pgonzalez@alivebox.com>
2013-07-10 16:12:25 -0600

    commit incomplete case details

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ App = Ember.Application.create();
 App.Router.map(function() {
     this.route("application", { path: "/" });
     this.route("dashboardDetail", { path: "/dashboardDetail" });
+    this.route("caseDetailsAnnoucebmml", { path: "/caseDetailsAnnoucebmml" });
 });
 
 App.ApplicationRoute = Ember.Route.extend({
@@ -10,7 +11,8 @@ App.ApplicationRoute = Ember.Route.extend({
         return cases;
     },
     setupController: function(controller, model){
-        controller.set('cases', model)
+        controller.set('cases', model),
+        controller.set('caseSelected', model[2])
     }
 });
 
@@ -23,10 +25,20 @@ App.DashboardDetailRoute = Ember.Route.extend({
     }
 });
 
+App.CaseDetailsAnnoucebmmlRoute = Ember.Route.extend({
+    model: function() {
+        return cases;
+    },
+    setupController: function(controller, model){
+        controller.set('caseDetails', model)
+    }
+});
+
 var cases = [
     {
         id: 0,
         caseNumber: '234086082309',
+        name:'Boland vs Yoccabel',
         description: 'Boland et al v. Yoccabel Construction Company, Inc.',
         parties: '10',
         documents:'5',
@@ -39,11 +51,19 @@ var cases = [
             {id:'0', description:'New party member: John Smith has been added'},
             {id:'1', description:'Scoott Justice, Court Clerk posted: The court will be closed monday'},
             {id:'2', description:'New party member: Carrie Carpenter has been added'}],
-        summary:{title:'B vs Y', days:'28 Days', description:'til next hearing'}
+        summary:{title:'B vs Y', days:'28 Days', description:'til next hearing'},
+        caseProfile:{
+            judge:'Hon. Reggie Holmes',
+            caseLiasion:'Charlene Balesteri',
+            caseNumber:'1234acbde',
+            caseCategory:'Civil Unlimited',
+            caseType:'Construction Defect',
+            courtLocation:'Orange country central Justic center'}
     },
     {
         id: 1,
         caseNumber: '435436-4745327-24',
+        name:'Tenempaguy vs Water Street',
         description: 'Tenempaguy v. 160 Water Street, Inc.',
         parties: '23',
         documents:'9',
@@ -56,11 +76,19 @@ var cases = [
             {id:'0', description:'New party member: John Smith has been added'},
             {id:'1', description:'Scoott Justice, Court Clerk posted: The court will be closed monday'},
             {id:'2', description:'New party member: Carrie Carpenter has been added'}],
-        summary:{title:'T vs W', days:'28 Days', description:'til next hearing'}
+        summary:{title:'T vs W', days:'28 Days', description:'til next hearing'},
+        caseProfile:{
+            judge:'Hon. Reggie Holmes',
+            caseLiasion:'Charlene Balesteri',
+            caseNumber:'1234acbde',
+            caseCategory:'Civil Unlimited',
+            caseType:'Construction Defect',
+            courtLocation:'Orange country central Justic center'}
     },
     {
         id: 2,
         caseNumber: '469346-3466',
+        name:'Jones vs Grey',
         description: 'Jones V. Grey',
         parties: '36',
         documents:'11',
@@ -73,7 +101,14 @@ var cases = [
             {id:'0', description:'New party member: John Smith has been added'},
             {id:'1', description:'Scoott Justice, Court Clerk posted: The court will be closed monday'},
             {id:'2', description:'New party member: Carrie Carpenter has been added'}],
-        summary:{title:'J vs G', days:'28 Days', description:'til next hearing'}
+        summary:{title:'J vs G', days:'28 Days', description:'til next hearing'},
+        caseProfile:{
+                    judge:'Hon. Reggie Holmes',
+                    caseLiasion:'Charlene Balesteri',
+                    caseNumber:'1234acbde',
+                    caseCategory:'Civil Unlimited',
+                    caseType:'Construction Defect',
+                    courtLocation:'Orange country central Justic center'}
     }
 
 ];;
